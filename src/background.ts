b8da62ed70883e7c59278a05d41383fd26be313e
Lars J. Aas <lars.j.aas@gmail.com>
2018-11-20 20:56:04 +0100

    #41: toggle toolbar icon color, and toggle popup setup

--- src/background.ts
+++ src/background.ts
@@ -6,3 +6,23 @@ function polling() {
 
 polling();
 
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.onRBKweb) {
+        chrome.pageAction.show(sender.tab.id);
+        chrome.pageAction.setPopup({
+            tabId: sender.tab.id,
+            popup: "popup.html"
+        });
+
+    }
+});
+
+chrome.tabs.onUpdated.addListener(function(tabId, info, tab) {
+    if (info.status === "loading") {
+        chrome.pageAction.hide(tabId);
+        chrome.pageAction.setPopup({
+            tabId: tabId, 
+            popup: ""
+        });
+    }
+});
