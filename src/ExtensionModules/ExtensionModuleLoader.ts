e03bb8e391e3cc0f67f517dfc2ece7262b693544
Rune Jacobsen <havremunken@gmail.com>
2018-11-22 20:21:45 +0100

    Module loader returns Brukertips module statically

--- src/ExtensionModules/ExtensionModuleLoader.ts
+++ src/ExtensionModules/ExtensionModuleLoader.ts
@@ -1,10 +1,15 @@
 import { ExtensionModule } from "./extensionmodule";
+import { Brukertips } from "./EM_Brukertips";
 
 /**
  * ExtensionModuleLoader
  * Loads all ExtensionModule classes from a path
  */
 
+// TODO: Last inn fra generert JSON-fil med liste over moduler (når den eksisterer)
+// TODO: Mat inn config fra sync storage til den enkelte modulen
 export default function loadModules(path: string): Array<ExtensionModule> {
-    return [];
+    return [
+        new Brukertips()
+    ];
 }
