4876248b0af486946a31411caadf58d079a01ba8
Lars J. Aas <lars.j.aas@gmail.com>
2018-11-26 00:48:14 +0100

    silence SignatureFilter, indentation

--- src/ExtensionModules/EM_SignatureFilter.ts
+++ src/ExtensionModules/EM_SignatureFilter.ts
@@ -31,39 +31,39 @@ export class SignatureFilter implements ExtensionModule {
     };
 
     execute = () => {
-        chrome.runtime.sendMessage({logMessage: "SignatureFilter"});
+        //chrome.runtime.sendMessage({logMessage: "SignatureFilter"});
         var elts = document.body.querySelectorAll("table.forumline tbody tr td table tbody tr td");
         elts.forEach(function(elt, key, parent) {
             try {
-            var sub = elt as HTMLTableCellElement;
-            var index = sub.textContent.indexOf("_________________");
-            if (index != -1) {
-                var remove = false;
-                sub.childNodes.forEach(function(node, key, parent) {
-                    var n = node as HTMLElement;
-                    var idx = n.innerHTML.indexOf("_________________");
-                    if (idx != -1) {
-                        // we have a .signature
-                        var belement = ((node as Element).closest("table").closest("tr") as HTMLTableRowElement).firstElementChild;
-                        var username = belement.querySelector("b").textContent;
-                        var postid = belement.querySelector("a").getAttribute("name");
-                        var trelement = belement.nextElementSibling as Element;
-                        trelement = belement.closest("tr").nextElementSibling as Element;
-                        var link = trelement.querySelectorAll('a[href*="profile.php"').item(0) as Element;
-                        var userid = parseInt(link.getAttribute("href").match(/u=([0-9]*)/)[1]);
-                        //chrome.runtime.sendMessage({logMessage: "removing signature for " + username + " (user " + userid + ")"});
-                        if (userid == 6289 /* larsarus - /* userid should be filtered */) {
-                            remove = true;
-                            n.innerHTML = n.innerHTML.substring(0, idx);
+                var sub = elt as HTMLTableCellElement;
+                var index = sub.textContent.indexOf("_________________");
+                if (index != -1) {
+                    var remove = false;
+                    sub.childNodes.forEach(function(node, key, parent) {
+                        var n = node as HTMLElement;
+                        var idx = n.innerHTML.indexOf("_________________");
+                        if (idx != -1) {
+                            // we have a .signature
+                            var belement = ((node as Element).closest("table").closest("tr") as HTMLTableRowElement).firstElementChild;
+                            var username = belement.querySelector("b").textContent;
+                            var postid = belement.querySelector("a").getAttribute("name");
+                            var trelement = belement.nextElementSibling as Element;
+                            trelement = belement.closest("tr").nextElementSibling as Element;
+                            var link = trelement.querySelectorAll('a[href*="profile.php"').item(0) as Element;
+                            var userid = parseInt(link.getAttribute("href").match(/u=([0-9]*)/)[1]);
+                            //chrome.runtime.sendMessage({logMessage: "removing signature for " + username + " (user " + userid + ")"});
+                            if (userid == 6289 /* larsarus - /* userid should be filtered */) {
+                                remove = true;
+                                n.innerHTML = n.innerHTML.substring(0, idx);
+                            }
+                        } else if (remove) {
+                            n.innerHTML = "";
                         }
-                    } else if (remove) {
-                        n.innerHTML = "";
-                    }
-                });
+                    });
+                }
+            } catch (e) {
+                chrome.runtime.sendMessage({message: e.message, exception: e});
             }
-        } catch (e) {
-            chrome.runtime.sendMessage({message: e.message, exception: e});
-        }
         });
     };
 };
