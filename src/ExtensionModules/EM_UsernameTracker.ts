680771f27693c40a62cca1ad4df6407276633a30
Lars J. Aas <lars.j.aas@gmail.com>
2018-11-30 23:54:30 +0100

    #36: inject html based on tracked username changes

--- src/ExtensionModules/EM_UsernameTracker.ts
+++ src/ExtensionModules/EM_UsernameTracker.ts
@@ -37,20 +37,46 @@ export class UsernameTracker implements ExtensionModule {
         }
     };
 
+    names: Map<number, string> = new Map<number, string>();
+
     execute = () => {
         var users = document.body.querySelectorAll('tr td span.name b')
         if (!users) return;
         users.forEach(function(elt, idx, parent) {
             var element = elt as HTMLElement;
+            var username = element.textContent;
             var row = element.closest("tr") as HTMLTableRowElement;
             var nextrow = row.nextElementSibling as HTMLTableRowElement;
-            var links = nextrow.querySelectorAll('td table tbody tr td a');
+            var links = nextrow.querySelectorAll('td table tr td a');
             // FIXME: something fishy is going on here - the 'table tbody tr td' part is not honored... :-/
-            var linkelt = links.item(1) as HTMLAnchorElement;
+            var linkelt = links.item(1) as HTMLAnchorElement; // should be item(0)!
             var link = linkelt.href;
-            var userid = link.replace(/.*\bu=/g, "");
-            element.setAttribute("data-userid", userid);
-            // chrome.runtime.sendMessage({logMessage: "user " + element.textContent + ", id " + userid});
-        });
+            var userid = parseInt(link.replace(/.*\bu=/g, ""));
+            if (!this.names[6500]) this.names[6500] = "Individual 1"; // DEBUG until stored settings
+            if (!this.names[6289]) this.names[6289] = "Individual 2"; // DEBUG until stored settings
+            if (!this.names[userid]) {
+                this.names[userid] = username;
+            }
+            else if (this.names[userid] && this.names[userid] != username) {
+                var span = element.closest('span') as HTMLElement;
+                span.insertAdjacentHTML('afterend', '<span class="nav aka' + userid +
+                                        '"><br>aka&nbsp;' + this.names[userid] +
+                                        '&nbsp;<a title="Thank you RUSK!" class="aka'+userid+'">' +
+                                        '<img src="' + chrome.runtime.getURL("/img/checkmark.png") +
+                                        '" width="12" height="12" border="0" valign="bottom" /></a></span>');
+                var anchor = (span.closest("td") as HTMLTableCellElement).querySelector('a.aka' + userid) as HTMLAnchorElement;
+                anchor.addEventListener('click', function(ev) {
+                    var spanelts = (span.closest("table") as HTMLTableElement).querySelectorAll('span.aka' + userid);
+                    for (var c = 0; c < spanelts.length; ++c) {
+                        var spanelt = spanelts.item(c) as HTMLSpanElement;
+                        spanelt.style.display = "none";
+                    }
+                    return true;
+                }.bind(this));
+            }
+            element.setAttribute("data-userid", "" + userid);
+            // chrome.runtime.sendMessage({logMessage: "user " + username +
+            //                             ", id " + userid});
+        }.bind(this));
     }
 };
