28f03313bb2a239b7fa9ebec2dc14d37ae06a32f
Lars J. Aas <lars.j.aas@gmail.com>
2018-12-13 18:49:53 +0100

    embed tweets via twitframe.com iframes

--- src/ExtensionModules/EM_MediaEmbedder.ts
+++ src/ExtensionModules/EM_MediaEmbedder.ts
@@ -37,14 +37,26 @@ export class MediaEmbedder implements ExtensionModule {
                     .WithDefaultValue(true)
                     .AsSharedSetting()
             )
+            .WithConfigOption(opt =>
+                opt
+                    .WithSettingName("EmbedTwitter")
+                    .WithLabel("Gjør om tweet-linker til tweets")
+                    .WithSettingType(SettingType.bool)
+                    .WithDefaultValue(true)
+                    .AsSharedSetting()
+            )
             .Build();
 
     posts: Array<PostInfo>;
+
+    // configs
     embedYoutube: boolean;
+    embedTwitter: boolean;
 
     init = (config: ModuleConfiguration) => {
         this.cfg = config;
         this.embedYoutube = true;
+        this.embedTwitter = true;
     }
 
     preprocess = () => {
@@ -54,24 +66,55 @@ export class MediaEmbedder implements ExtensionModule {
     execute = () => {
         this.posts.forEach(function(post: PostInfo, idx, posts) {
             post.rowElement.querySelectorAll('a').forEach(function(anchor: HTMLAnchorElement, key, parent) {
-                if (anchor.parentElement.tagName.match(/SPAN/i) && anchor.parentElement.classList.contains("postbody")) {
-                    var href: string = anchor.href;
-                    if (this.embedYoutube && (href.match(/youtube\.com/i) || href.match(/youtu\.be/i))) {
-                        //console.log("found: " + href);
-                        var match = href.match(/https?:\/\/(m\.|www\.)?youtube\.com\/watch\/([a-zA-Z0-9]*)/);
-                        if (!match) match = href.match(/https?:\/\/(m\.|www\.)?youtube\.com\/watch\?v=([a-zA-Z0-9]*)/);
-                        if (!match) match = href.match(/https?:\/\/(youtu)\.be\/([^\/?#]*)/);
-                        if (match) {
-                            var code = match[2];
-                            //console.log("youtube code: " + code);
-                            anchor.insertAdjacentHTML('afterend', '<br>' +
-                                '<object width="460" height="270" data="https://www.youtube.com/embed/' + code +
-                                         '" frameborder="0" allow="encrypted-media"></object>');
-                            anchor.classList.add("RUSKHiddenItem"); // option for hiding link?
+                try {
+                    if (anchor.parentElement.tagName.match(/SPAN/i) && anchor.parentElement.classList.contains("postbody")) {
+                        var href: string = anchor.href;
+                        // console.log("link: " + href);
+                        if (this.embedYoutube && (href.match(/youtube\.com/i) || href.match(/youtu\.be/i))) {
+                            // console.log("found: " + href);
+                            var match = href.match(/https?:\/\/(m\.|www\.)?youtube\.com\/watch\/([a-zA-Z0-9]*)/);
+                            if (!match) match = href.match(/https?:\/\/(m\.|www\.)?youtube\.com\/watch\?v=([a-zA-Z0-9]*)/);
+                            if (!match) match = href.match(/https?:\/\/(youtu)\.be\/([^\/?#]*)/);
+                            if (match) {
+                                var code = match[2];
+                                // console.log("youtube code: " + code);
+                                anchor.insertAdjacentHTML('afterend', '<br>' +
+                                    '<object width="460" height="270" data="https://www.youtube.com/embed/' + code +
+                                             '" frameborder="0" allow="encrypted-media"></object>');
+                                anchor.classList.add("RUSKHiddenItem"); // option for hiding link?
+                            }
+                        }
+                        else if (this.embedTwitter && href.match(/twitter\.com\/.*status\/([0-9]*)/i)) {
+                            // console.log("found: " + href);
+                            var match = href.match(/twitter\.com\/.*\/status\/([0-9]*)/i);
+                            if (match) {
+                                var code = match[1];
+                                anchor.insertAdjacentHTML('afterend', '<br>' +
+                                    '<iframe id="tweet_' + code + '" border=0 frameborder=0 width=460 src="https://twitframe.com/show?url=' + encodeURI(href) + '"></iframe>');
+                                anchor.classList.add("RUSKHiddenItem"); // option for hiding link?
+                            }
                         }
                     }
+                } catch (e) {
+                    console.error("execute exception: " + e.message);
                 }
             }.bind(this));
         }.bind(this));
+
+        var tweets = document.body.querySelectorAll('iframe[id^="tweet_"]');
+        tweets.forEach(function(tweetframe: HTMLIFrameElement, key, parent) {
+            tweetframe.addEventListener('load', function() {
+                tweetframe.contentWindow.postMessage({ element: tweetframe.id, query: "height" },
+                                                     "https://twitframe.com");
+            });
+        });
+
+        window.addEventListener("message", function(event) {
+            if (event.origin != "https://twitframe.com") return;
+            if (event.data && event.data.height && event.data.element.match(/^tweet_/)) {
+                var iframe = document.getElementById(event.data.element) as HTMLIFrameElement;
+                iframe.style.height = parseInt(event.data.height) + "px";
+            }
+        });
     };
 };
