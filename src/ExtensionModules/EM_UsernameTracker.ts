87635a1492828b6794596b9a7b4e10ae3bccaff6
Lars J. Aas <lars.j.aas@gmail.com>
2018-12-26 22:31:20 +0100

    UsernameTracker: PostInfo-based rewrite

--- src/ExtensionModules/EM_UsernameTracker.ts
+++ src/ExtensionModules/EM_UsernameTracker.ts
@@ -4,19 +4,19 @@ import { RBKwebPageType } from "../Context/RBKwebPageType";
 import { ConfigBuilder } from "../Configuration/ConfigBuilder";
 import { ConfigurationOptionVisibility } from "../Configuration/ConfigurationOptionVisibility";
 import { ModuleConfiguration } from "../Configuration/ModuleConfiguration";
+import { PostInfo } from "../Utility/PostInfo";
+import { Log } from "../Utility/Log";
 
 /**
  * EM_UsernameTracker - Extension module for RBKweb.
  */
-
-// FIXME: switch to use PostInfo extraction
 
 export class UsernameTracker implements ExtensionModule {
     readonly name : string = "UsernameTracker";
     cfg: ModuleConfiguration;
 
     pageTypesToRunOn: Array<RBKwebPageType> = [
-        RBKwebPageType.RBKweb_FORUM_POSTLIST // FIXME: only post views
+        RBKwebPageType.RBKweb_FORUM_POSTLIST
     ];
 
     runBefore: Array<string> = ['late-extmod'];
@@ -48,65 +48,62 @@ export class UsernameTracker implements ExtensionModule {
             var count = 0;
             for (var key in this.names) { count += 1; }
             var logmsg = this.name + ": tracking " + count + " account names (" + dictstr.length + " bytes)";
-            console.log(logmsg);
-            chrome.runtime.sendMessage({logMessage: logmsg, level: "debug"});
+            Log.Debug(logmsg);
+            if (dictstr.length > 7500) Log.Warning("UsernameTracker closing in on 8K limit!");
         } catch (e) {
             console.error(this.name + " init failed: " + e.message);
         }
     }
 
+    posts: Array<PostInfo>;
+
     preprocess = () => {
-        var users = document.body.querySelectorAll('tr td span.name b')
-        if (!users) return;
-        var updated = false;
-        users.forEach(function(elt, idx, parent) {
-            var element = elt as HTMLElement;
-            var username = element.textContent;
-            var row = element.closest("tr") as HTMLTableRowElement;
-            var nextrow = row.nextElementSibling as HTMLTableRowElement;
-            var links = nextrow.querySelectorAll('td table tr td a');
-            // FIXME: something fishy is going on here - the 'table tbody tr td' part is not honored... :-/
-            var linkelt = links.item(1) as HTMLAnchorElement; // should be item(0)!
-            var link = linkelt.href;
-            var userid = parseInt(link.replace(/.*\bu=/g, ""));
-            element.setAttribute("data-userid", "" + userid);
-            if (!this.names[userid]) {
-                this.names[userid] = username;
-                updated = true;
+        try {
+            this.posts = PostInfo.GetPostsFromDocument(document);
+            var updated = false;
+            this.posts.forEach(function(post: PostInfo, idx, posts) {
+                var username = post.posterNickname;
+                var userid = post.posterid;
+                if (userid != -1 && !this.names[userid]) {
+                    this.names[userid] = username;
+                    updated = true;
+                }
+            }.bind(this));
+            if (updated) { // we found some new names
+                this.storeKnownUsernames();
             }
-        }.bind(this));
-        if (updated) {
-            this.storeKnownUsernames();
+        } catch (e) {
+            console.error("error finding usernames: " + e.message + " - " + e.stack);
         }
     }
 
     execute = () => {
-        var users = document.body.querySelectorAll('tr td span.name b');
-        if (!users) return;
-        users.forEach(function (elt, idx, parent) {
-            var element = elt as HTMLElement;
-            var username = element.textContent;
-            var userid = parseInt(element.getAttribute("data-userid"));
-            if (this.names[userid] && this.names[userid] != username) {
-                var span = element.closest('span') as HTMLElement;
-                span.insertAdjacentHTML('afterend', '<span class="nav aka' + userid +
-                                        '"><br>aka&nbsp;' + this.names[userid] +
-                                        '&nbsp;<a title="Thank you RUSK!" class="aka'+userid+'">' +
-                                        '<img src="' + chrome.runtime.getURL("/img/checkmark.png") +
-                                        '" width="12" height="12" border="0" valign="bottom" /></a></span>');
-                var anchor = (span.closest("td") as HTMLTableCellElement).querySelector('a.aka' + userid) as HTMLAnchorElement;
-                anchor.addEventListener('click', function(ev) {
-                    var spanelts = (span.closest("table") as HTMLTableElement).querySelectorAll('span.aka' + userid);
-                    for (var c = 0; c < spanelts.length; ++c) {
-                        var spanelt = spanelts.item(c) as HTMLSpanElement;
-                        spanelt.style.display = "none";
-                    }
-                    this.names[userid] = username;
-                    this.storeKnownUsernames();
-                    return true;
-                }.bind(this));
-            }
-        }.bind(this));
+        try {
+            this.posts.forEach(function(post: PostInfo, idx, posts) {
+                var username = post.posterNickname;
+                var userid = post.posterid;
+                if (userid == -1) return;
+                if (this.names[userid] && this.names[userid] != username) {
+                    var spanelt = post.rowElement.querySelector('td span.name') as HTMLSpanElement;
+                    spanelt.insertAdjacentHTML('afterend', '<span class="nav aka' + userid +
+                                                '"><br>aka&nbsp;' + this.names[userid] +
+                                                '&nbsp;<a title="Thank you RUSK!" class="aka'+userid+'">' +
+                                                '<img src="' + chrome.runtime.getURL("/img/checkmark.png") +
+                                                '" width="12" height="12" border="0" valign="bottom" /></a></span>');
+                    var anchor = (spanelt.closest("td") as HTMLTableCellElement).querySelector('a.aka' + userid) as HTMLAnchorElement;
+                    anchor.addEventListener('click', function(ev) {
+                        var table = spanelt.closest('table') as HTMLTableElement;
+                        table.querySelectorAll('span.aka' + userid).forEach(function(elt: HTMLSpanElement, idx, parent) {
+                            elt.style.display = 'none';
+                        }.bind(this));
+                        this.names[userid] = username;
+                        this.storeKnownUsernames();
+                    }.bind(this));
+                }
+            }.bind(this));
+        } catch (e) {
+            console.error("exception: " + e.message + " - " + e.stack);
+        }
     }
 
     private getConfigItem(setting: string): string {
@@ -121,4 +118,4 @@ export class UsernameTracker implements ExtensionModule {
         var dictstr = JSON.stringify(this.names);
         this.cfg.ChangeSetting("knownUsernames", dictstr);
     }
-};
+}
