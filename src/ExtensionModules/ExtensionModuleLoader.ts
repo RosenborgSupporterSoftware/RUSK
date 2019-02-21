25c28b327d3b700d37449d05791067d70d4a1048
Rune Jacobsen <havremunken@gmail.com>
2019-02-21 08:59:47 +0100

    Remove old, unused colorize modules

--- src/ExtensionModules/ExtensionModuleLoader.ts
+++ src/ExtensionModules/ExtensionModuleLoader.ts
@@ -3,15 +3,12 @@ import { Usertips } from "./EM_Usertips";
 import { TabTitles } from "./EM_TabTitles";
 import { SeasonViews } from "./EM_SeasonViews";
 import { SignatureFilter } from "./EM_SignatureFilter";
-// import { ColorizeThreads } from "./EM_ColorizeThreads";
 import { ImageCache } from "./EM_ImageCache";
 import { InboxAlert } from "./EM_InboxAlert";
 import { TopicFilter } from "./EM_TopicFilter";
 import { HighlightColor } from "./EM_HighlightColor";
 import { UsernameTracker } from "./EM_UsernameTracker";
 import { Empowerment } from "./EM_Empowerment";
-// import { ColorizeForums } from "./EM_ColorizeForums";
-// import { ColorizePosts } from "./EM_ColorizePosts";
 import { UserFilter } from "./EM_UserFilter";
 import { EnhancePosting } from "./EM_EnhancePosting";
 import { ThreadTitleLinks } from "./EM_ThreadTitleLinks";
@@ -55,9 +52,6 @@ export default function loadModules(path: string): Array<ExtensionModule> {
         new AvatarFilter(),
         new ThreadTitleLinks(),
         new Colorize(),
-        // new ColorizeThreads(),
-        // new ColorizeForums(),
-        // new ColorizePosts(),
         new AlternateSearchEngine(),
         new Bookmarks(),
         new QuickReply(),
