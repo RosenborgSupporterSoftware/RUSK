c6e6748118ebcd628d15097e770fac2534f506a5
Lars J. Aas <lars.j.aas@gmail.com>
2019-04-14 22:25:20 +0200

    tabtitles update for norwegian language

--- src/ExtensionModules/EM_TabTitles.ts
+++ src/ExtensionModules/EM_TabTitles.ts
@@ -30,18 +30,37 @@ export class TabTitles extends ModuleBase {
                 titleelt.textContent = title.substr(22);
             else if (title.startsWith("RBKweb - View Forum - "))
                 titleelt.textContent = title.substr(22);
+            else if (title.startsWith("RBKweb - Forum - "))
+                titleelt.textContent = title.substr(17);
+            else if (title.startsWith("RBKweb - forum - "))
+                titleelt.textContent = title.substr(17);
             else if (title.startsWith("RBKweb - Index"))
                 titleelt.textContent = "Index";
+            else if (title.startsWith("RBKweb - Hovedsiden"))
+                titleelt.textContent = "Hovedsiden";
             else if (title.startsWith("RBKweb - Search"))
                 titleelt.textContent = "Search";
+            else if (title.startsWith("RBKweb - Søk"))
+                titleelt.textContent = "Søk";
             else if (title.startsWith("RBKweb - Private Messaging"))
                 titleelt.textContent = "Private Messaging";
+            else if (title.startsWith("RBKweb - Private meldinger"))
+                titleelt.textContent = "Private meldinger";
             else if (title.startsWith("RBKweb - Send private message"))
                 titleelt.textContent = "Send private message";
             else if (title.startsWith("RBKweb - Read message"))
                 titleelt.textContent = "Read message";
             else if (title.indexOf("RBKweb - Kamper sesongen ") != -1)
                 titleelt.textContent = title.substring(title.indexOf(" - ") + 3);
+            else if (title.startsWith("RBKweb - Forumstatistikk"))
+                titleelt.textContent = "Forumstatistikk";
+            else if (title.startsWith("RBKweb - Endre din profil"))
+                titleelt.textContent = "Endre din profil";
+            else
+                console.log("title '" + title + "' did not match any rewrite pattern");
+        }
+        else {
+            console.log("head.title == null");
         }
     }
 };
